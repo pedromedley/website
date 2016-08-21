41fbb5dc1b8c58e9bb234fb47a80a782601faf54
Gregory Schier <gschier1990@gmail.com>
2016-08-21 12:30:02 -0700

    Better download button

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,33 +1,38 @@
 (function () {
     var els = document.querySelectorAll('.__download-link');
+
+    var prefix = 'Download for ';
+    var location = null;
+    var platform = null;
+
+    if (navigator.platform.toLowerCase().indexOf('mac') !== -1) {
+        platform = 'Mac';
+        location = '/download#mac';
+    } else if (navigator.platform.toLowerCase().indexOf('win') !== -1) {
+        platform = 'Windows';
+        location = '/download#windows';
+    } else if (navigator.platform.toLowerCase().indexOf('linux') !== -1) {
+        platform = 'Linux';
+        location = '/download#ubuntu';
+    }
+
     for (var i = 0; i < els.length; i++) {
         var el = els[i];
-        var prefix = 'Download for ';
-        var defaultText = 'Download Insomnia';
-        var platform = null;
 
         if (el.getAttribute('data-verbose')) {
             prefix = 'Download Insomnia for ';
         }
 
-        if (navigator.platform.toLowerCase().indexOf('mac') !== -1) {
-            platform = 'Mac';
-            el.innerHTML = 'Download for Mac';
-            el.setAttribute('href', '/download#mac');
-        } else if (navigator.platform.toLowerCase().indexOf('win') !== -1) {
-            el.innerHTML = 'Download for Windows';
-            platform = 'Windows';
-            el.setAttribute('href', '/download#windows');
-        } else if (navigator.platform.toLowerCase().indexOf('linux') !== -1) {
-            platform = 'Linux';
-            el.innerHTML = 'Download for Linux';
-            el.setAttribute('href', '/download#ubuntu');
-        }
-
         if (platform) {
             el.innerHTML = prefix + platform;
-        } else {
-            el.innerHTML = defaultText;
+        }
+
+
+        if (location) {
+            el.onclick = function (e) {
+                e.preventDefault();
+                window.location = location;
+            };
         }
     }
 })();
