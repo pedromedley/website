2fa7c46caafc2d02dd30dec288cbeb9b9d4e6497
Gregory Schier <gschier1990@gmail.com>
2016-10-18 10:03:55 -0700

    Something

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,38 +1,39 @@
 (function () {
-    var els = document.querySelectorAll('.__download-link');
+  var els = document.querySelectorAll('.__download-link');
 
-    var prefix = 'Download Free for ';
-    var location = null;
-    var platform = null;
+  var prefix = 'Download Free for ';
+  var location = null;
+  var platform = null;
 
-    if (navigator.platform.toLowerCase().indexOf('mac') !== -1) {
-        platform = 'Mac';
-        location = '/download#mac';
-    } else if (navigator.platform.toLowerCase().indexOf('win') !== -1) {
-        platform = 'Windows';
-        location = '/download#windows';
-    } else if (navigator.platform.toLowerCase().indexOf('linux') !== -1) {
-        platform = 'Linux';
-        location = '/download#ubuntu';
+  if (navigator.platform.toLowerCase().indexOf('mac') !== -1) {
+    platform = 'Mac';
+    location = '/download#mac';
+  } else if (navigator.platform.toLowerCase().indexOf('win') !== -1) {
+    platform = 'Windows';
+    location = '/download#windows';
+  } else if (navigator.platform.toLowerCase().indexOf('linux') !== -1) {
+    platform = 'Linux';
+    location = '/download#ubuntu';
+  }
+
+  for (var i = 0; i < els.length; i++) {
+    var el = els[i];
+
+    if (el.getAttribute('data-verbose')) {
+      prefix = 'Download Insomnia for ';
     }
 
-    for (var i = 0; i < els.length; i++) {
-        var el = els[i];
-
-        if (el.getAttribute('data-verbose')) {
-            prefix = 'Download Insomnia for ';
-        }
-
-        if (platform) {
-            el.innerHTML = prefix + platform;
-        }
-
-
-        if (location) {
-            el.onclick = function (e) {
-                e.preventDefault();
-                window.location = location;
-            };
-        }
+    if (platform) {
+      el.innerHTML = prefix + platform;
     }
+
+
+    if (location) {
+      el.onclick = function (e) {
+        e.preventDefault();
+        window.location = location;
+      };
+    }
+  }
 })();
+
